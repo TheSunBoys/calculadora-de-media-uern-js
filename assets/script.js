21971443daa081133e63740c080b09a4fc1d25ec
Thiago Frank <luffyzao1000@gmail.com>
2023-07-25 20:32:37 -0300

    Sistema Pronto

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+function calcular() {
+    let n1 = parseFloat(document.querySelector('#nota1').value);
+    let n2 = parseFloat(document.querySelector('#nota2').value);
+    let n3 = parseFloat(document.querySelector('#nota3').value);
+    const resultado = document.querySelector('span');
+    const aviso = document.querySelector('#aviso');
+  
+    if (!isNaN(n1) && !isNaN(n2) && !isNaN(n3)) {
+      n1 = n1 * 4;
+      n2 = n2 * 5;
+      n3 = n3 * 6;
+      let media = (n1 + n2 + n3) / 15;
+
+      if (media<7 && media>4){
+        let recuperacao = 12 - media;
+        let texto = `Você precisa de ${recuperacao} na quarta prova.`
+
+        aviso.textContent = texto;
+      } else if (media<4){
+        texto = "Você está reprovado!"
+
+        aviso.textContent = texto;
+      }
+      
+  
+      resultado.innerHTML = media;
+    } else {
+      resultado.innerHTML = "Preencha todas as notas corretamente.";
+    }   
+}
+
+  
+  
+
